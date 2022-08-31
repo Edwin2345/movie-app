e2af1861b05d007f930432baf11cb9b4d33f91af
unknown <edjoseph226@gmail.com>
2022-08-31 22:09:56 +0530

    Title and Search Bar Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from "react"
+import {useEffect} from "react"
 import './App.css';
+import SearchIcon from "./search.svg"
+
+const API_URL = "http://www.omdbapi.com?apikey=f36ce219";
 
 function App() {
+
+  //Fetch movies
+  const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`)
+      const data = await response.json()
+
+      console.log(data.Search)
+  }
+
+  useEffect( () => {
+     searchMovies('Spiderman')
+  }, [])
+
   return (
-    <h1>Movie Land</h1>
+    <div className="app">
+      {/*Title*/}
+       <h1>MovieLand</h1>
+
+       {/*Search Bar */}
+       <div className="search">
+         <input 
+            placeholder="Search for movies" 
+            value="Superman" 
+            onChange={() => {}}
+          />
+        <img
+         src={SearchIcon}
+         alt="search"
+         onClick={() => {}}
+        />
+       </div>
+
+    </div>
   );
 }
 
