e5b1ee11ecccc1c6c659bbc730fa972ac9a6de61
Edwin Joseph <edjoseph226@gmail.com>
2023-04-10 01:05:40 -0400

    modal can close now when click outside content

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -15,62 +15,72 @@ function Modal({closeModal, title, movie, year, score, genre}) {
   else if(movie.release_date){
     date = movie.release_date;
   }
+
   
   return (
     <>
-          <div
-            className="justify-center my-3 items-center overflow-x-hidden  fixed inset-0 z-50 outline-none focus:outline-none"
+        <div
+      className="fixed top-0 bottom-0 left-0 right-0 bg-[rgba(0,0,0,0.4)] flex justify-center items-center z-[1]"
+      onClick={() => {
+        // close modal when outside of modal is clicked
+        closeModal();
+      }}
+    >
+      <div className="justify-center my-3 items-center overflow-x-hidden  fixed inset-0 outline-none focus:outline-none">
+        <div className="relative w-[70%] md:w-[60%] lg:w-[40%] mx-auto max-h-[95%] my-6 overflow-y-auto">
+          {/*content*/}
+          <div className="text-white bg-slate-600 border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none" 
+            onClick={(e)=>{
+              e.stopPropagation();
+            }}
           >
-            <div className="relative w-[70%] md:w-[60%] lg:w-[40%] mx-auto max-h-[95%] my-6 overflow-y-auto">
-              {/*content*/}
-              <div className="text-white bg-slate-600 border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-center justify-center py-2 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-[1.3rem] md:text-[1.6rem] lg:text-[1.9rem] text-center font-semibold">
-                    {`${title} (${year})`}
-                  </h3>
-                </div>
-                {/* image */}
-                <div className="flex items-center justify-center p-5  border-b border-solid border-slate-200 rounded-t">
-                <img src={imgSrc}  onError={() => {
-                   setImgSrc(errorImage)
-                 }}
-                 alt={`${movie.title}`}
-               />
-                </div>
-                {/*Summary*/}
-                <div className="relative px-6 py-0 flex-auto">
-                  <p className="my-2 text-white text-[0.7rem] md:text-[0.9rem] lg:text-[0.95rem] text-justify leading-relaxed">
-                    {movie.overview}
-                  </p>
-                  {/* Info */}
-                  <div className="flex flex-col text-left text-[0.75rem] md:text-[0.95rem] lg:text-[1.2rem] text-yellow-500 pb-2 rounded-t">
-                    <p>
-                      {`Released:\u00A0\u00A0\u00A0${date}`}
-                    </p>
-                    <p>
-                      {`Rating:\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${score}`}
-                    </p>
-                    <p>
-                      {`Genre:\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${genre}`}
-                    </p>
-                  </div>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-center py-2 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="lg:text-[1.2rem] bg-red-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => closeModal()}
-                  >
-                    Close
-                  </button>
-                </div>
+            {/*header*/}
+            <div className="flex items-center justify-center py-2 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-[1.3rem] md:text-[1.6rem] lg:text-[1.9rem] text-center font-semibold">
+                {`${title} (${year})`}
+              </h3>
+            </div>
+            {/* image */}
+            <div className="flex items-center justify-center p-5  border-b border-solid border-slate-200 rounded-t">
+            <img src={imgSrc}  onError={() => {
+                setImgSrc(errorImage)
+              }}
+              alt={`${movie.title}`}
+            />
+            </div>
+            {/*Summary*/}
+            <div className="relative px-6 py-0 flex-auto">
+              <p className="my-2 text-white text-[0.7rem] md:text-[0.9rem] lg:text-[0.95rem] text-justify leading-relaxed">
+                {movie.overview}
+              </p>
+              {/* Info */}
+              <div className="flex flex-col text-left text-[0.75rem] md:text-[0.95rem] lg:text-[1.2rem] text-yellow-500 pb-2 rounded-t">
+                <p>
+                  {`Released:\u00A0\u00A0\u00A0${date}`}
+                </p>
+                <p>
+                  {`Rating:\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${score}`}
+                </p>
+                <p>
+                  {`Genre:\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0${genre}`}
+                </p>
               </div>
             </div>
+            {/*footer*/}
+            <div className="flex items-center justify-center py-2 border-t border-solid border-slate-200 rounded-b">
+              <button
+                className="lg:text-[1.2rem] bg-red-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => closeModal()}
+              >
+                Close
+              </button>
+            </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
+        </div>
+      </div>
+    </div>
+  </>
   );
 }
 
